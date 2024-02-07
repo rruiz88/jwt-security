8f0e4efe8398396c689c2462c80ad372d0683d04
rruiz88 <roman.ruiz816@gmail.com>
2024-02-07 13:19:59 -0600

    controller and config completed

---
+++ demo/src/main/java/dev/romanruiz/demo/auth/AuthRequest.java
@@ -0,0 +1,18 @@
+package dev.romanruiz.demo.auth;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequest {
+
+    private String email;
+
+    String password;
+}
---
+++ demo/src/main/java/dev/romanruiz/demo/auth/AuthService.java
@@ -0,0 +1,50 @@
+package dev.romanruiz.demo.auth;
+
+
+import dev.romanruiz.demo.config.JwtService;
+import dev.romanruiz.demo.user.Role;
+import dev.romanruiz.demo.user.User;
+import dev.romanruiz.demo.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+
+    private final UserRepository repository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    public AuthResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthResponse.builder().token(jwtToken).build();
+    }
+
+    public AuthResponse authenticate(AuthRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        var user = repository.findByEmail(request.getEmail()).orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthResponse.builder().token(jwtToken).build();
+    }
+}
